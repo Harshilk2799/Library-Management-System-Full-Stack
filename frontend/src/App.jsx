892078d148edbd8ee914796857d1562d2e19aa7a
Harshil Khatri <khatriharshil26@gmail.com>
2026-02-02 15:21:48 +0530

    Code Splitting (Faster load time)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,124 +1,133 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
 import Header from "./components/Header";
-import AdminLogin from "./pages/admin/AdminLogin";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import AdminDashboard from "./pages/admin/AdminDashboard";
-import AddCategory from "./pages/admin/AddCategory";
-import ManageCategory from "./pages/admin/ManageCategory";
+import Loader from "./components/Loader";
 import AdminProtectedRoute from "./pages/auth/AdminProtectedRoute";
-import AddAuthor from "./pages/admin/AddAuthor";
-import ManageAuthor from "./pages/admin/ManageAuthor";
-import AddBook from "./pages/admin/AddBook";
-import ManageBook from "./pages/admin/ManageBook";
-import AdminChangePassword from "./pages/admin/AdminChangePassword";
-import UserSignup from "./pages/student/UserSignup";
-import UserLogin from "./pages/student/UserLogin";
-import StudentDashboard from "./pages/student/StudentDashboard";
 import StudentProtectedRoute from "./pages/auth/StudentProtectedRoute";
-import StudentBooks from "./pages/student/StudentBooks";
-import StudentProfile from "./pages/student/StudentProfile";
-import StudentChangePassword from "./pages/student/StudentChangePassword";
+
+const AdminLogin = lazy(() => import("./pages/admin/AdminLogin"));
+const AdminDashboard = lazy(() => import("./pages/admin/AdminDashboard"));
+const AddCategory = lazy(() => import("./pages/admin/AddCategory"));
+const ManageCategory = lazy(() => import("./pages/admin/ManageCategory"));
+const AddAuthor = lazy(() => import("./pages/admin/AddAuthor"));
+const ManageAuthor = lazy(() => import("./pages/admin/ManageAuthor"));
+const AddBook = lazy(() => import("./pages/admin/AddBook"));
+const ManageBook = lazy(() => import("./pages/admin/ManageBook"));
+const AdminChangePassword = lazy(() =>
+  import("./pages/admin/AdminChangePassword")
+);
+const UserSignup = lazy(() => import("./pages/student/UserSignup"));
+const UserLogin = lazy(() => import("./pages/student/UserLogin"));
+const StudentDashboard = lazy(() => import("./pages/student/StudentDashboard"));
+const StudentBooks = lazy(() => import("./pages/student/StudentBooks"));
+const StudentProfile = lazy(() => import("./pages/student/StudentProfile"));
+const StudentChangePassword = lazy(() =>
+  import("./pages/student/StudentChangePassword")
+);
 
 function App() {
   return (
     <BrowserRouter>
       <ToastContainer autoClose={2000} />
       <Header />
-      <Routes>
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route
-          path="/admin/add-category"
-          element={
-            <AdminProtectedRoute>
-              <AddCategory />
-            </AdminProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/manage-category"
-          element={
-            <AdminProtectedRoute>
-              <ManageCategory />
-            </AdminProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/add-author"
-          element={
-            <AdminProtectedRoute>
-              <AddAuthor />
-            </AdminProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/manage-author"
-          element={
-            <AdminProtectedRoute>
-              <ManageAuthor />
-            </AdminProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/add-book"
-          element={
-            <AdminProtectedRoute>
-              <AddBook />
-            </AdminProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/manage-book"
-          element={
-            <AdminProtectedRoute>
-              <ManageBook />
-            </AdminProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/change-password"
-          element={
-            <AdminProtectedRoute>
-              <AdminChangePassword />
-            </AdminProtectedRoute>
-          }
-        />
-        <Route path="/user/signup" element={<UserSignup />} />
-        <Route path="/user/login" element={<UserLogin />} />
-        <Route
-          path="/user/dashboard"
-          element={
-            <StudentProtectedRoute>
-              <StudentDashboard />
-            </StudentProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/books"
-          element={
-            <StudentProtectedRoute>
-              <StudentBooks />
-            </StudentProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/profile"
-          element={
-            <StudentProtectedRoute>
-              <StudentProfile />
-            </StudentProtectedRoute>
-          }
-        />
-        <Route
-          path="/user/change-password"
-          element={
-            <StudentProtectedRoute>
-              <StudentChangePassword />
-            </StudentProtectedRoute>
-          }
-        />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+          <Route
+            path="/admin/add-category"
+            element={
+              <AdminProtectedRoute>
+                <AddCategory />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/manage-category"
+            element={
+              <AdminProtectedRoute>
+                <ManageCategory />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/add-author"
+            element={
+              <AdminProtectedRoute>
+                <AddAuthor />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/manage-author"
+            element={
+              <AdminProtectedRoute>
+                <ManageAuthor />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/add-book"
+            element={
+              <AdminProtectedRoute>
+                <AddBook />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/manage-book"
+            element={
+              <AdminProtectedRoute>
+                <ManageBook />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/change-password"
+            element={
+              <AdminProtectedRoute>
+                <AdminChangePassword />
+              </AdminProtectedRoute>
+            }
+          />
+          <Route path="/user/signup" element={<UserSignup />} />
+          <Route path="/user/login" element={<UserLogin />} />
+          <Route
+            path="/user/dashboard"
+            element={
+              <StudentProtectedRoute>
+                <StudentDashboard />
+              </StudentProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/books"
+            element={
+              <StudentProtectedRoute>
+                <StudentBooks />
+              </StudentProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/profile"
+            element={
+              <StudentProtectedRoute>
+                <StudentProfile />
+              </StudentProtectedRoute>
+            }
+          />
+          <Route
+            path="/user/change-password"
+            element={
+              <StudentProtectedRoute>
+                <StudentChangePassword />
+              </StudentProtectedRoute>
+            }
+          />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
